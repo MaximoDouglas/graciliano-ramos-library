0e8fef739be4f6d04764e8dcdd1a72547d9d65b2
latived <vitalwarley@gmail.com>
2019-02-25 16:39:11 -0300

    Add camera horizontal movement
    - With 'w' and 's' we can move forward or backward, respectively.
    - With 'r' and 'R' we move the camera to the right and left,
    respectively.
    - With 'q' we quit the program.
    - Add a few global vars to control these actions, as explained above.
    - Modified `rotacionate`, now called `navigate`, to wrap the matrix control with
    `gluPerspective` and `gluLookAt`.
    - Create a new method to receive keyboard input and call `navigate`.

--- code/main.py
+++ code/main.py
@@ -1,39 +1,57 @@
+import sys
+import math
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from simple_objects import Objects as obj
 
-degree = 0
 rotate = False
 rotateLeft = False;
+
+degree = 0
+radius = 50.0
+rad = math.pi/180
+
+width = 0
+height = 0
+aspect_ratio = 0
+
 
 def init():
 	glClearColor(0.0, 0.0, 0.0, 0.0)
 	glEnable(GL_DEPTH_TEST)
 
+
 def reshape(w, h):
+    global width
+    global height
+    global aspect_ratio
+
+    width = float(w)
+    height = float(h)
+    aspect_ratio = width / height
+
     glViewport(0, 0, w, h)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    gluPerspective(45, w/h, 0.01, 100)
-    gluLookAt(0.9, 0.5, 1.8, 0.0, 0.5, 0.0, 0.0, 1.0, 0.0);
+    gluPerspective(45, aspect_ratio, 0.01, 100)
+    gluLookAt(30.0, 30.0, 30.0, 0.0, 0.5, 0.0, 0.0, 1.0, 0.0);
 
-def rotacionate():
-    global degree
 
-    if(rotate):
-        if (rotateLeft):
-            degree = (degree + 1) % 360
-        else:
-            degree = (degree - 1) % 360
+def navigate():
+    glLoadIdentity()
+    gluPerspective(45.0, aspect_ratio, 0.01, 100.0)
+    gluLookAt(radius * math.sin(degree * rad), 0, radius * math.cos(degree * rad),
+               0.0, 0.5, 0.0,
+               0.0, 1.0, 0.0)
+
 
 def draw_scenario():
 
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
-	rotacionate()
 	glPushMatrix()
-	glRotatef(degree, 0.0, 1.0, 0.0)
+
 	obj.draw_floors()
 	obj.draw_tops()
 	obj.draw_walls()
@@ -43,6 +61,8 @@ def draw_scenario():
 	glPopMatrix()
 	glutSwapBuffers()
 
+
+# TODO: implement in `navigate`
 def OnMouseClick(button, state, x, y):
     global rotate
     global rotateLeft
@@ -56,6 +76,31 @@ def OnMouseClick(button, state, x, y):
     if button == GLUT_MIDDLE_BUTTON:
         rotate = False
 
+
+def keyboard(key, x, y):
+    global degree
+    global radius
+
+    key = key.decode("utf-8")
+
+    if key == 'r':
+        degree = (degree + 5) % 360
+        navigate()
+    elif key == 'R':
+        degree = (degree - 5) % 360
+        navigate()
+    elif key == 'w':
+        radius -= 3
+        navigate()
+    elif key == 's':
+        radius += 3
+        navigate()
+    elif key == 'q':
+        sys.exit()
+
+    glutPostRedisplay()
+
+
 def main():
     glutInit()
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
@@ -64,9 +109,12 @@ def main():
     glutCreateWindow("Graciliano Ramos Library")
     init()
     glutDisplayFunc(draw_scenario)
+    glutKeyboardFunc(keyboard)
     glutIdleFunc(draw_scenario)
     glutMouseFunc(OnMouseClick)
     glutReshapeFunc(reshape)
     glutMainLoop()
 
+
 main()
+
