3817fc6bc928c6be89f23aedb3e16cba12720fa9
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-03-04 21:45:07 -0300

    Table class comments and variables names refactored

--- code/tables.py
+++ code/tables.py
@@ -4,12 +4,12 @@ class Table:
 
     def __init__(self, reference=None):
         self.reference = reference #x, y and z of the floor
-        #A for iteration for each one of the seven parts that compose the chair
+        #A for iteration for each one of the seven parts that compose the table
         self.sub_part_half_width = [0.50, 0.01, 0.01, 0.01, 0.01]
         self.sub_part_half_height = [0.05, 0.275, 0.275, 0.275, 0.275]
         self.sub_part_half_depth = [0.50, 0.01, 0.01, 0.01, 0.01]
         self.sub_part_center = self.__get_sub_part_center(self.reference)
-        self.chair = []
+        self.table = []
 
         self.__generate_coordinates()
 
@@ -77,38 +77,38 @@ class Table:
 
     def __generate_coordinates(self):
 
-        # For loop to generate all the coordinates of the chair parts
+        # For loop to generate all the coordinates of the table parts
 
         for i in range(len(self.sub_part_half_height)):
             sub_part = []
 
             a, b, c, d, e, f, g, h = self.__gen_vertexes(i)
 
-            # This vector stores the vertices of the front face of the sub_part part of the chair
+            # This vector stores the vertices of the front face of the sub_part part of the table
             sub_part_front = (a,b,c,d)
             sub_part.append(sub_part_front)
 
-            # This vector stores the vertices of the sub_part face of the sub_part part of the chair
+            # This vector stores the vertices of the sub_part face of the sub_part part of the table
             sub_part_back = (e,f,g,h)
             sub_part.append(sub_part_back)
 
-            # This vector stores the vertices of the right face of the sub_part part of the chair
+            # This vector stores the vertices of the right face of the sub_part part of the table
             sub_part_right = (b,f,g,c)
             sub_part.append(sub_part_right)
 
-            # This vector stores the vertices of the left face of the sub_part part of the chair
+            # This vector stores the vertices of the left face of the sub_part part of the table
             sub_part_left = (e,a,d,h)
             sub_part.append(sub_part_left)
 
-            # This vector stores the vertices of the top face of the sub_part part of the chair
+            # This vector stores the vertices of the top face of the sub_part part of the table
             sub_part_top = (c,g,h,d)
             sub_part.append(sub_part_top)
 
-            # This vector stores the vertices of the bottom face of the sub_part part of the chair
+            # This vector stores the vertices of the bottom face of the sub_part part of the table
             sub_part_bottom = (a,e,f,b)
             sub_part.append(sub_part_bottom)
 
-            self.chair.append(sub_part)
+            self.table.append(sub_part)
 
     def get_table(self):
-            return self.chair
+            return self.table
