fa89ea1b912d9be37da49d06584f64f24e645509
latived <vitalwarley@gmail.com>
2019-03-06 00:19:06 -0300

    Add support to two cameras
    - Camera 1 can be used with the `r` and `R` keys. It is centered at
    origin and its radius is defined at 50.
    - Camera 2 can be used normally with the keys: `w`, `s` and all arrow
    keys.
    - The left and right arrow keys rotate the center around the eye's
    y-axis.
    - The up and down arrow keys rotate only the first two quadrants around
    the eye's x-axis.
    
    All that was described above is in `keyboard` and `update_view`.
    
    One problem left to fix is to modify the radius without moving the
    center.

--- code/main.py
+++ code/main.py
@@ -5,19 +5,19 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from simple_objects import Objects as obj
 
-rotate = False
-rotateLeft = False;
-
-az_degree = 0
-el_degree = 20
-radius = 50.0
-rad = math.pi/180
-
 width = 0
 height = 0
 aspect_ratio = 0
 
-center = [0.0, 0.0, 0.0]
+rad = math.pi/180
+radius = 5.0
+eye_degree = [0, 20]  # in relation to center positon
+center_degree = [180, 0]  # in relation to eye position
+origin_centered = False
+
+# debug
+eye = None
+ctr = None
 
 def init():
     glClearColor(0.0, 0.0, 0.0, 0.0)
@@ -38,14 +38,34 @@ def reshape(w, h):
 
     update_view()
 
+
 def update_view():
+    global eye
+    global ctr
+
+    eyex = radius * math.sin(eye_degree[0] * rad)
+    eyey = radius * math.sin(eye_degree[1] * rad)
+    eyez = radius * math.cos(eye_degree[0] * rad)
+    cx = radius * math.sin(center_degree[0] * rad) + eyex
+    cy = radius * math.sin(center_degree[1] * rad) + eyey
+    cz = radius * math.cos(center_degree[0] * rad) + eyez
+
+    if origin_centered:
+        eyex = 50 * math.sin(eye_degree[0] * rad)
+        eyey = 50 * math.sin(eye_degree[1] * rad)
+        eyez = 50 * math.cos(eye_degree[0] * rad)
+        cx, cy, cz = 0, 0, 0
+
+    # debug
+    eye = (round(eyex, 3), round(eyey, 3), round(eyez, 3))
+    ctr = (round(cx, 3), round(cy, 3), round(cz, 3))
+
     glLoadIdentity()
     gluPerspective(45.0, aspect_ratio, 0.01, 100.0)
-    gluLookAt(radius * math.sin(az_degree * rad),
-              radius * math.sin(el_degree * rad),
-              radius * math.cos(az_degree * rad),
-              center[0], center[1], center[2],
+    gluLookAt(eyex, eyey, eyez,
+              cx, cy, cz,
               0.0, 1.0, 0.0)
+
 
 def draw_scenario():
 
@@ -61,19 +81,22 @@ def draw_scenario():
     obj.draw_doors()
     #obj.draw_chairs()
     #obj.draw_tables()
-    obj.draw_book_cases()
+    #obj.draw_book_cases()
 
     alt = 50.0
     alt_rev = -5
-    axis((0.0, 0.0, alt), (1.0, 0.0, 0.0)) # z to red
-    axis((0.0, alt, 0.0), (0.0, 1.0, 0.0)) # y to green
-    axis((alt, 0.0, 0.0), (0.0, 0.0, 1.0)) # x to blue
-    axis((0.0, 0.0, alt_rev), (0.0, 0.0, 0.0)) # -z to black
-    axis((0.0, alt_rev, 0.0), (0.0, 0.0, 0.0)) # -y to black
-    axis((alt_rev, 0.0, 0.0), (0.0, 0.0, 0.0)) # -x to black
+    axis(ctr, (ctr[0], ctr[1], alt), (1.0, 0.0, 0.0)) # z to red
+    axis(ctr, (ctr[0], alt, ctr[2]), (0.0, 1.0, 0.0)) # y to green
+    axis(ctr, (alt, ctr[1], ctr[2]), (0.0, 0.0, 1.0)) # x to blue
+    axis(ctr, (ctr[0], ctr[1], alt_rev), (1.0, 1.0, 0.0)) # -z to yellow
+    axis(ctr, (ctr[0], alt_rev, ctr[2]), (1.0, 0.0, 1.0)) # -y to pink
+    axis(ctr, (alt_rev, ctr[1], ctr[2]), (0.0, 1.0, 1.0)) # -x to cyan
+    # line at (0,15,0)
+    axis((0,0,0),(0,15,0),(0.0,0.0,0.0))
 
     glPopMatrix()
     glutSwapBuffers()
+
 
 def get_world_coords(x, y, verbose=1):
 
@@ -99,9 +122,9 @@ def get_world_coords(x, y, verbose=1):
         print("Stencil: ", obj.dnames[index[0][0]])
         print("World coords: ", [round(wc, 3) for wc in world_coords])
 
-    center[0] = world_coords[0]/radius
-    center[1] = -world_coords[1]/radius
-    center[2] = world_coords[2]/radius
+    center_degree[0] = world_coords[0]/radius
+    center_degree[1] = -world_coords[1]/radius
+    center_degree[2] = world_coords[2]/radius
 
 
 def mouse_motion(x, y):
@@ -115,30 +138,45 @@ def mouse_click(button, state, x, y):
     get_world_coords(x, y)
     glutPostRedisplay()
 
+
 def keyboard(key, x, y):
-    global az_degree
-    global el_degree
+    global eye_degree
+    global eye_degree
     global radius
+    global center_degree
+    global origin_centered
 
     if not isinstance(key, int):
         key = key.decode("utf-8")
 
+    print('--- distance eye to center_degree ---')
+    print('radius ', radius)
+    print('center ', ctr)
+    print('eye    ', eye)
+    print('dist   ', math.sqrt((eye[0] - ctr[0])**2 +
+                               (eye[1] - ctr[1])**2 +
+                               (eye[2] - ctr[2])**2))
+
+    origin_centered = False
+
     if key == GLUT_KEY_DOWN:
-        print("DOWN")
+        center_degree[1] = (center_degree[1] + 5) % 360
     elif key == GLUT_KEY_UP:
-        print("UP")
+        center_degree[1] = (center_degree[1] - 5) % 360
     elif key == GLUT_KEY_LEFT:
-        print("LEFT")
+        center_degree[0] = (center_degree[0] + 5) % 360
     elif key == GLUT_KEY_RIGHT:
-        print("RIGHT")
+        center_degree[0] = (center_degree[0] - 5) % 360
     elif key == 'r':
-        az_degree = (az_degree + 5) % 360
+        origin_centered = True
+        eye_degree[0] = (eye_degree[0] + 5) % 360
     elif key == 'R':
-        az_degree = (az_degree - 5) % 360
+        origin_centered = True
+        eye_degree[0] = (eye_degree[0] - 5) % 360
     elif key == 'e':
-        el_degree = (el_degree + 5) % 360
+        eye_degree[1] = (eye_degree[1] + 5) % 360
     elif key == 'E':
-        el_degree = (el_degree - 5) % 360
+        eye_degree[1] = (eye_degree[1] - 5) % 360
     elif key == 'w':
         radius -= 3
     elif key == 's':
@@ -150,10 +188,10 @@ def keyboard(key, x, y):
 
 
 # just for debug purposes :)
-def axis(last_vertex, c):
+def axis(center, last_vertex, c):
     glColor(c)
     glBegin(GL_LINES)
-    glVertex((0.0, 0.0, 0.0))
+    glVertex(center)
     glVertex(last_vertex)
     glEnd()
 
